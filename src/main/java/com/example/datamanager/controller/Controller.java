18ba881e3ab903056dfef3c9f4987ccd17b29874
Bob <>
2024-01-18 23:40:44 +0200

    [DM-000] removes empty line

--- src/main/java/com/example/datamanager/controller/Controller.java
+++ src/main/java/com/example/datamanager/controller/Controller.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-
   private final DataProvider dataProvider;
   private final ResponseMapper responseMapper;
 
